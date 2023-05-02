f3929f51d8fc1920735fb0f2b65b7667d5a9220a
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-05-02 12:59:39 +0300

    task7

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,7 +3,7 @@ import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import d from './Modal.module.css';
 
-const modalRoot = document.querySelector('#root');
+const modalRoot = document.querySelector('#modal-root');
 
 class Modal extends Component {
   componentDidMount() {
